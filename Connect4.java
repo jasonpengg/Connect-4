e2693863ae15d9450d96e40bb4541a303aae7051
Pengguinn <130693366+Pengguinn@users.noreply.github.com>
2023-12-25 16:35:18 -0500

    Added Graphics and Mouse Motion

--- Connect4.java
+++ Connect4.java
@@ -2,26 +2,76 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
+import java.io.*;
 
-public class Connect4 implements ActionListener{
+public class Connect4 implements ActionListener, MouseListener, MouseMotionListener{
 	//Properties 
-	JFrame theframe = new JFrame("graphics!!!");
-	ConnectPanel thepanel = new ConnectPanel();
-	Timer thetimer = new Timer(1000/30, this);
+	JFrame theFrame = new JFrame("graphics!!!");
+	ConnectPanel thePanel = new ConnectPanel();
+	JTextArea chatArea = new JTextArea();
+	JScrollPane theScroll = new JScrollPane(chatArea);
 
+	Timer theTimer = new Timer(1000/30, this);
+	SuperSocketMaster ssm = null;
+
+
+	/*
+	HOW THE CODE WILL WORK: 
+	Mouse listener and MouseMotionLsitener will use booleans to dictate where the piece will be dropped/final place 
+	The mouseReleased will be the final coords of where the piece can go and that will start an action of calculations 
+	Calcuations include: placing a piece into an array, changing player turns, checking for connect 4s, and SSM related stuff 
+	 */
 	// Methods
+	public void mouseMoved(MouseEvent evt){
+		
+	}
+	public void mouseDragged(MouseEvent evt){
+		thePanel.intDraggedX = evt.getX();
+		thePanel.intDraggedY = evt.getY();
+		//System.out.println("Drag = " + thePanel.intDraggedX +" "+thePanel.intDraggedY);
+	}
+	public void mouseExited(MouseEvent evt){
+	
+	}
+	public void mouseEntered(MouseEvent evt){
+	
+	}
+	public void mouseReleased(MouseEvent evt){
+
+	}
+	public void mousePressed(MouseEvent evt){
+		thePanel.intPressedX = evt.getX();
+		thePanel.intPressedY = evt.getX();
+		System.out.println("Pressed = " +thePanel.intPressedX +" "+ thePanel.intPressedY);
+
+	}
+	public void mouseClicked(MouseEvent evt){
+		
+	}
 	public void actionPerformed(ActionEvent evt){
 
 	}
 	// Constructor
+	//add timer to the panel to repaint 
 	public Connect4(){
-		thepanel.setPreferredSize(new Dimension(1280, 720));
-		theframe.setContentPane(thepanel);
-		thepanel.repaint();
-		theframe.pack();
-		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		theframe.setVisible(true);
-		thetimer.start();
+		thePanel.setPreferredSize(new Dimension(1280, 720));
+		thePanel.setLayout(null);
+		theFrame.setContentPane(thePanel);
+		
+		theScroll.setSize(480,400);
+		theScroll.setLocation(10,310);
+		thePanel.add(theScroll);
+
+
+		theFrame.addMouseListener(this);
+		theFrame.addMouseMotionListener(this);
+		
+		thePanel.repaint();
+		theFrame.pack();
+		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.setResizable(false);
+		theFrame.setVisible(true);
+		theTimer.start();
 	}
 
 	// Main Method
--- ConnectPanel.java
+++ ConnectPanel.java
@@ -7,29 +7,83 @@ import javax.imageio.*;
 
 public class ConnectPanel extends JPanel{
 	// Properties 
-
-
+	boolean blnPrintGrid = true;
+	int intPressedX = 0;
+	int intPressedY = 0; 
+	int intDraggedX = 0;
+	int intDraggedY = 0;
+	int intRedX = 390; 
+	int intRedY = 210;
+	int intYellowX = 390;
+	int intYellowY = 110;
+	
 	// Methods
 	// override how JComponent is painted 
+	// Set the Red and Yellow pieces to coordinates 
 	public void paintComponent(Graphics g){
 	g.setColor(Color.BLUE);
-	g.fillRect(500,90,770,610);
+	g.fillRect(500,100,770,610);
+	
+	g.setColor(Color.WHITE);
+	int intX = 400;
+	int intY = 10;
+	if(blnPrintGrid = true){
+		for (int intCount = 0; intCount < 7; intCount++){
+			intX = intX +110;
+			for(int intCount2 = 0; intCount2 < 6; intCount2++){
+				intY = intY + 100;
+				g.fillOval(intX,intY, 90, 90);
+			}
+			intY = 10;
+		}
+		blnPrintGrid = false;
+	}
+	//RED
+	System.out.println(intPressedX);
+	if(intPressedX > (390) && intPressedX < (intRedX +90) && intPressedY > intRedY && intPressedY < (intRedY + 90)){
+		System.out.println("true");
+		g.setColor(Color.BLUE);
+		g.fillRect(500,100,770,610);
+		
+	}
 	
 	g.setColor(Color.RED);
-	//Horizontal 
-	g.fillOval(510,600, 90, 90);
-	g.fillOval(620,600, 90, 90);
-	g.fillOval(730,600, 90, 90);
-	g.fillOval(840,600, 90, 90);
-	g.fillOval(950,600, 90, 90);
-	g.fillOval(1060,600, 90, 90);
-	g.fillOval(1170,600, 90, 90);
+	g.fillOval(intRedX,intRedY, 90, 90);
+	g.setColor(Color.YELLOW);
+	g.fillOval(intYellowX,intYellowY, 90, 90);
+	
+	
+	g.setColor(Color.GRAY);
+	g.fillRect(500,0,1,800);
+	g.fillRect(610,0,1,800);
+	g.fillRect(720,0,1,800);
+	g.fillRect(830,0,1,800);
+	g.fillRect(940,0,1,800);
+	g.fillRect(1050,0,1,800);
+	g.fillRect(1160,0,1,800);
+	g.fillRect(1270,0,1,800);
+	}
+	//Constructor
+	public ConnectPanel (){
+
+		
+	}
+}
+/* IDEAS FOR HOW THE GRID IS ORGANIZED
+ * //Horizontal 
+	g.fillOval(510,610, 90, 90);
+	g.fillOval(620,610, 90, 90);
+	g.fillOval(730,610, 90, 90);
+	g.fillOval(840,610, 90, 90);
+	g.fillOval(950,610, 90, 90);
+	g.fillOval(1060,610, 90, 90);
+	g.fillOval(1170,610, 90, 90);
 	//Vertical 
-	g.fillOval(510,500, 90, 90);
-	g.fillOval(510,400, 90, 90);
-	g.fillOval(510,300, 90, 90);
-	g.fillOval(510,200, 90, 90);
-	g.fillOval(510,100, 90, 90);
+	g.fillOval(510,510, 90, 90);
+	g.fillOval(510,410, 90, 90);
+	g.fillOval(510,310, 90, 90);
+	g.fillOval(510,210, 90, 90);
+	g.fillOval(510,110, 90, 90);
 
 	g.setColor(Color.GRAY);
 	g.fillRect(500,0,1,800);
@@ -40,11 +94,4 @@ public class ConnectPanel extends JPanel{
 	g.fillRect(1050,0,1,800);
 	g.fillRect(1160,0,1,800);
 	g.fillRect(1270,0,1,800);
-	
-	}
-	//Constructor
-	public ConnectPanel (){
-
-		
-	}
-}
+	*/
